a91e85693dc43d86cd1d1d1a3ec68549c9b5a002
sebas <sebas@Scorpion.sebas.com>
2021-01-21 21:52:09 -0300

    Final clase 2

--- Clase2/resources/js/main.js
+++ Clase2/resources/js/main.js
@@ -9,28 +9,74 @@ const
     /**
      * Campo de búsqueda
      */
-    busqueda = document.querySelector('#busqueda'),
+    busqueda = document.querySelector('#txtBuscar'),
     /**
      * Convertir una tarea en HTML
      */
-	tareaTemplate = ({ nombre, descripcion }) => `
-    <li> 
-    <div class="collapsible-header">
-          <div>
-              <i class="material-icons">event_note</i>
-              ${nombre}
-          </div>
-          <div>
-              <a href="#">
-                  <i class="material-icons">edit</i>
-              </a>
-              <a href="#">
-                  <i class="material-icons">delete_forever</i>
-              </a>
-          </div>
-      </div>
-    <div class="collapsible-body"><span>${descripcion}</span></div>
-  </li>`,
+    tareaTemplate = ({ nombre, descripcion }) => {
+        const li = document.createElement('li');
+
+        const divHeader = document.createElement('div');
+        divHeader.className = 'collapsible-header';
+
+        const divHeaderDiv1 = document.createElement('div');
+
+        const iHeader = document.createElement('i');
+        iHeader.className = 'material-icons';
+        iHeader.innerHTML = 'event_note';
+
+        divHeaderDiv1.appendChild(iHeader);
+        divHeaderDiv1.innerHTML += nombre;
+
+
+        const divHeaderDiv2 = document.createElement('div');
+
+        const aEdit = document.createElement('a');
+        aEdit.href = '#';
+
+        aEdit.onclick = function() {
+            alert('Editandoo');
+        }
+
+        const iEdit = document.createElement('i');
+        iEdit.className = 'material-icons';
+        iEdit.innerHTML = 'edit';
+        aEdit.appendChild(iEdit);
+
+        const aDelete = document.createElement('a');
+        aDelete.href = '#';
+
+        aDelete.onclick = function() {
+            vmLista.borrar(nombre);
+        }
+
+        const iDelete = document.createElement('i');
+        iDelete.className = 'material-icons';
+        iDelete.innerHTML = 'delete_forever';
+        aDelete.appendChild(iDelete);
+
+        divHeaderDiv2.appendChild(aEdit);
+        divHeaderDiv2.appendChild(aDelete);
+
+        divHeader.appendChild(divHeaderDiv1);
+        divHeader.appendChild(divHeaderDiv2);
+
+
+        const divBody = document.createElement('div');
+        divBody.className = 'collapsible-body';
+
+        const span = document.createElement('span');
+        span.innerHTML = descripcion;
+        divBody.appendChild(span);
+
+
+        li.appendChild(divHeader);
+        li.appendChild(divBody);
+
+
+        return li;
+    }
+
   /**
    * Estructura de datos que representa la lista de tareas
    * lista = [
@@ -50,14 +96,19 @@ const
    */
   vmLista = {
 
-    mapItemsToHTML(listaAux = false){
+    mapItemsToHTML(listaAux = 0) {
 
-        const dataSource = listaAux != false ? listaAux : lista;
+        const dataSource = listaAux != 0 ? listaAux : lista;
 
         listaTareas.innerHTML = '';
+
+        const content = document.createDocumentFragment();
+
         dataSource.forEach(function(item){
-            listaTareas.innerHTML += tareaTemplate(item);
-        })
+            content.appendChild(tareaTemplate(item));
+        });
+
+        listaTareas.appendChild(content);
     },
 
     agregarItem(tarea) {
@@ -73,20 +124,25 @@ const
         })
 
         return result;
+    },
+
+    borrar(nombre) {
+        lista = lista.filter(function(item){
+            return item.nombre != nombre
+        });
+        this.mapItemsToHTML();
     }
 
   }
 
-let
-    modalBusqueda = false;
 
 // Call-Stack
 function DOM_Load() {
 	M.Sidenav.init(document.querySelectorAll(".sidenav"), {});
 	M.Collapsible.init(document.querySelectorAll(".collapsible"), {});
 
-	modalBusqueda = M.Modal.init(
-		document.querySelector("#modalBusqueda"),
+	M.Modal.init(
+		document.querySelector("#modalAgregar"),
 		{}
 	);
 
@@ -102,18 +158,21 @@ function registerListeners() {
         
     busqueda.addEventListener('keypress', busqueda_KeyPress);
 
-    document
-        .querySelector('#modalTrigger')
-        .addEventListener('click', modalTrigger_Click);
 }
 
 function btnAgregar_Click(e) {
-	e.preventDefault();
+    e.preventDefault();
+    
+    const nombreEl = document.querySelector('#nombreNuevaTarea');
+    const descEl = document.querySelector('#descripcionNuevaTarea');
 
 	vmLista.agregarItem({
-		nombre: "Terminar main.js X2",
-		descripcion: "Terminar el modulo bla bla bla",
-	});
+		nombre: nombreEl.value,
+		descripcion: descEl.value,
+    });
+    
+    nombreEl.value = '';
+    descEl.value = '';
 }
 
 function busqueda_KeyPress(e) {
@@ -123,10 +182,5 @@ function busqueda_KeyPress(e) {
         )
 
         e.target.value = '';
-        modalBusqueda.close();
     }
-}
-
-function modalTrigger_Click() {
-    document.getElementById("busqueda").focus();
 }
